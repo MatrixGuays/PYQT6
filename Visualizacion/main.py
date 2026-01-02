b3791c74367ad987b7f765b883ade64d5efffdd0
Nahuel Ojeda <matrixguays@gmail.com>
2026-01-02 10:33:45 -0300

    add style visuals

--- Visualizacion/main.py
+++ Visualizacion/main.py
@@ -3,14 +3,18 @@ import os
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QLabel, QPushButton, QDockWidget, QStatusBar, QTabWidget, QWidget, QHBoxLayout, QVBoxLayout, QListWidget, QFileDialog, QListWidgetItem)
 
 from PyQt6.QtGui import QPixmap, QAction, QKeySequence, QIcon
-from PyQt6.QtCore import Qt, QStandardPaths
+from PyQt6.QtCore import Qt, QStandardPaths, QSize
 
 class MainWindow (QMainWindow):
+    
     def __init__(self):
         super().__init__()
         self.initialize_ui()
         self.status_bar = QStatusBar()
         self.setStatusBar(self.status_bar)
+        with open("estilos.css", "r") as file:
+            style = file.read()
+        self.setStyleSheet(style)
     
     def initialize_ui(self):
         self.setGeometry (100, 100, 800, 500)
@@ -20,7 +24,6 @@ class MainWindow (QMainWindow):
         self.create_menu()
         self.create_dock()
         self.show()
-    
     def generate_main_window(self):
         tab_bar = QTabWidget(self)
         self.reproductor_container = QWidget()
@@ -50,11 +53,33 @@ class MainWindow (QMainWindow):
         song_image.setScaledContents(True)
         
         #botones
-        buttons_repeat = QPushButton("🚨")
-        buttons_before = QPushButton("🏛")
-        buttons_play = QPushButton ("💵")
-        buttons_next = QPushButton("🛠")
-        buttons_radom = QPushButton ("📍")
+        buttons_repeat = QPushButton()
+        buttons_repeat.setObjectName("buttons_repeat")
+        buttons_repeat.setIcon(QIcon("Images/repeat.png"))
+        buttons_repeat.setIconSize(QSize(24, 24))
+        buttons_before = QPushButton()
+        buttons_before.setObjectName("buttons_before")
+        buttons_before.setIcon(QIcon("Images/izquierda.png"))
+        buttons_before.setIconSize(QSize(24, 24))
+        buttons_play = QPushButton ()
+        buttons_play.setObjectName("buttons_play")
+        buttons_play.setIcon(QIcon("Images/play.jpeg"))
+        buttons_play.setIconSize(QSize(34, 24))
+        buttons_next = QPushButton()
+        buttons_next.setObjectName("buttons_next")
+        buttons_next.setIcon(QIcon("Images/siguiente.jpeg"))
+        buttons_next.setIconSize(QSize(24, 24))
+        buttons_radom = QPushButton()
+        buttons_radom.setObjectName("buttons_radom")
+        buttons_radom.setIcon(QIcon("Images/random.jpeg"))
+        buttons_radom.setIconSize(QSize(24, 24))
+        
+        
+        buttons_repeat.setFixedSize(40, 40)
+        buttons_before.setFixedSize (40, 40)
+        buttons_play.setFixedSize(50, 50)
+        buttons_next.setFixedSize(40, 40)
+        buttons_radom.setFixedSize(40, 40)
         
         buttons_h_box.addWidget(buttons_repeat)
         buttons_h_box.addWidget(buttons_before)
@@ -112,11 +137,11 @@ class MainWindow (QMainWindow):
     def open_folder_music(self):
         initial_dir = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.MusicLocation)
         selected_folder = QFileDialog.getExistingDirectory(None, "selecciona una carpeta", initial_dir)
-        icon = QIcon("images/image.png")
+        icon = QIcon("Images/image.png")
         
         for archivo in os.listdir(selected_folder):
             ruta_archivo = os.path.join (selected_folder, archivo)
-            if ruta_archivo.endwith(".txt"):
+            if ruta_archivo.endswith(".txt"):
                 item = QListWidgetItem(archivo)
                 item.setIcon(icon)
                 self.songs_list.addItem(item)
